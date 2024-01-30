7a81dceb99ba309ab10aea09e016c1b58506f3a6
SireCourage001 <sirecourage@gmail.com>
2024-01-30 14:25:20 -0800

    load_bal

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,62 @@
+#Install HAProxy package
+package { 'haproxy':
+  ensure => installed,
+
+}
+
+
+#Custom config for HAProxy
+file { '/etc/haproxy/haproxy_custom.cfg':
+  ensure  => present,
+  content => @("EOF"
+frontend http_front
+    http-response set-header X-Served-By $HOSTNAME
+EOF
+),
+ require => Package['haproxy'],
+  notify  => Service['haproxy'],
+}
+
+# Include custom configuration in the HAProxy main configuration file
+exec { 'include_custom_config':
+  command => 'echo "import /etc/haproxy/haproxy_custom.cfg" >> /etc/haproxy/haproxy.cfg',
+  creates => '/etc/haproxy/haproxy_custom.cfg',
+  require => File['/etc/haproxy/haproxy_custom.cfg'],
+  notify  => Service['haproxy'],
+}
+
+# Enable HAProxy init script
+file { '/etc/default/haproxy':
+  ensure  => present,
+  content => "ENABLED=1",
+require => Package['haproxy'],
+  notify  => Service['haproxy'],
+}
+
+# Update /etc/hosts with correct hostnames and IP addresses
+file { '/etc/hosts':
+  content => @("EOF"
+127.0.0.1 localhost
+52.87.229.242 443353-web-01
+52.3.241.114 443353-web-02
+# ... (other existing entries)
+EOF
+),
+  require => Package['haproxy'],
+}
+
+# Validate HAProxy configuration before restart
+exec { 'validate_haproxy_config':
+  command => 'haproxy -c -f /etc/haproxy/haproxy.cfg',
+  path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+  refreshonly => true,
+  subscribe => File['/etc/haproxy/haproxy.cfg'],
+  notify  => Service['haproxy'],
+}
+
+#OA Restart HAProxy service
+service { 'haproxy':
+  ensure    => running,
+  enable    => true,
+  subscribe => [File['/etc/haproxy/haproxy_custom.cfg'], File['/etc/hosts'], Exec['include_custom_config'], Exec['validate_haproxy_config']],
+}
